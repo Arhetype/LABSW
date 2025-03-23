9fc622b1e3fd88c92dd527a2961c1e836c0f73cb
Daniil <daniil.riabtcev@gmail.com>
2025-03-23 18:43:13 +0400

    fix: Lab3 Husky

--- backend/eslint.config.mjs
+++ backend/eslint.config.mjs
@@ -1,34 +1,49 @@
 import globals from 'globals';
 import pluginJs from '@eslint/js';
 import tseslint from 'typescript-eslint';
-import pluginPrettier from 'eslint-plugin-prettier'; // Плагин Prettier
+import pluginPrettier from 'eslint-plugin-prettier';
 import prettierConfig from 'eslint-config-prettier';
-import eslintPluginImport from 'eslint-plugin-import'; // Плагин для работы с импортами
-import eslintImportResolverTypescript from 'eslint-import-resolver-typescript';
+import eslintPluginImport from 'eslint-plugin-import';;
 
-/** @type {import('eslint').Linter.Config[]} */
+/** @type {import('eslint').Linter.Config} */
 export default [
-  { files: ['**/*.{js,mjs,cjs,ts}'] },
-  { languageOptions: { globals: globals.browser } },
+  {
+    files: ['**/*.{js,mjs,cjs,ts}'],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+  },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
-  prettierConfig, // Подключен eslint-config-prettier
   {
+    ...prettierConfig,
     plugins: {
-      prettier: pluginPrettier, // Подключен плагин Prettier
-      import: eslintPluginImport, // Подключен плагин для работы с импортами
+      prettier: pluginPrettier,
+      import: eslintPluginImport,
     },
     rules: {
       '@typescript-eslint/no-unused-vars': ['error'],
       '@typescript-eslint/explicit-function-return-type': 'off',
-      'prettier/prettier': 'error', // Правило для Prettier
-      'import/no-unresolved': 'error', // Проверка на неразрешенные импорты
-      'import/extensions': ['error', 'ignorePackages'], // Игнорирование расширений файлов
+      'prettier/prettier': 'error',
+      'import/no-unresolved': 'error',
+      'import/extensions': [
+        'error',
+        'ignorePackages',
+        {
+          js: 'never',
+          jsx: 'never',
+          ts: 'never',
+          tsx: 'never',
+        },
+      ],
     },
     settings: {
       'import/resolver': {
         typescript: {
-          project: './tsconfig.json', // Указываем путь к tsconfig.json
+          project: './tsconfig.json',
         },
       },
     },
